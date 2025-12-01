f3a38f6344dfe42c0cab5045d2c08146e102fd25
Breno <srbentu@gmail.com>
2025-12-01 20:14:15 +0100

    add OrbitControls to ThreePage and implement mouse movement tracking; add CSS reset styles

--- pages/three/index.js
+++ pages/three/index.js
@@ -1,6 +1,7 @@
-import gsap from "gsap";
 import { useEffect, useRef } from "react";
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+//import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 export default function ThreePage() {
   const canvasRef = useRef(null);
@@ -14,10 +15,17 @@ export default function ThreePage() {
     // Scene
     const scene = new THREE.Scene();
 
+    const cursor = { x: 0, y: 0 };
+
+    window.addEventListener("mousemove", (event) => {
+      cursor.x = event.clientX / window.innerWidth - 0.5;
+      cursor.y = -(event.clientY / window.innerHeight - 0.5);
+    });
+
     /**
      * Object
      */
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const geometry = new THREE.BoxGeometry(1, 1, 1, 5, 5, 5);
     const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
     const mesh = new THREE.Mesh(geometry, material);
     scene.add(mesh);
@@ -26,16 +34,28 @@ export default function ThreePage() {
      * Sizes
      */
     const sizes = {
-      width: 800,
-      height: 600,
+      width: window.innerWidth,
+      height: window.innerHeight,
     };
 
     /**
      * Camera
      */
-    const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-    camera.position.z = 3;
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      sizes.width / sizes.height,
+      1,
+      100,
+    );
+    camera.position.z = 2;
+    // camera.position.y = 2;
+    // camera.position.x = 2;
+    // camera.lookAt(mesh.position);
     scene.add(camera);
+
+    //CONTROLS
+    const controls = new OrbitControls(camera, canvas);
+    controls.enableDamping = true;
 
     /**
      * Renderer
@@ -47,12 +67,12 @@ export default function ThreePage() {
 
     //const clock = new THREE.Clock();
 
-    gsap.to(mesh.rotation, {
-      y: Math.PI * 2,
-      duration: 2,
-      repeat: -1,
-      ease: "linear",
-    });
+    // gsap.to(mesh.rotation, {
+    //   y: Math.PI * 1,
+    //   duration: 2,
+    //   repeat: -1,
+    //   ease: "linear",
+    // });
 
     //Animations
     const tick = () => {
@@ -61,6 +81,9 @@ export default function ThreePage() {
       //update objects
       //mesh.rotation.y = Math.sin(elapsedTime);
       //mesh.rotation.x = Math.cos(elapsedTime);
+
+      //Update controls
+      controls.update();
 
       //Render
       renderer.render(scene, camera);
@@ -78,7 +101,11 @@ export default function ThreePage() {
 
   return (
     <>
-      <canvas ref={canvasRef} className="webgl" />
+      <canvas
+        style={{ margin: "0", padding: "0" }}
+        ref={canvasRef}
+        className="webgl"
+      />
     </>
   );
 }
