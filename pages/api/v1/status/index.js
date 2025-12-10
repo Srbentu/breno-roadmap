e1f56d5daacf7b96e758e3b4269732e5ddd7ee69
Breno <srbentu@gmail.com>
2025-12-10 18:13:06 +0100

    remove unused database import and commented-out code in status API

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,6 @@
-import database from "/infra/database.js";
-
 async function status(req, res) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
+  //const result = await database.query("SELECT 1 + 1 as sum;");
+  //console.log(result.rows);
   res.status(200).json({ status: "são foda" });
 }
 
