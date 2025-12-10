22e9ad46a31bef0ae6ec6533f7d45a50e7b67c79
Breno <srbentu@gmail.con>
2025-12-10 19:57:48 +0100

    finish `/status` . endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,13 @@
 test("get to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3001/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(parsedUpdatedAt).toEqual(responseBody.updated_at);
+
+  expect(responseBody.dependnencies.database.postgress_version).toBe("16.0");
+  expect(responseBody.dependnencies.database.max_connections).toBe(100);
+  expect(responseBody.dependnencies.database.opened_connections).toBe(1);
 });
