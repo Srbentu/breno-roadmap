951158645fcadca09db1abdc14d5582bbb459165
Breno <srbentu@gmail.con>
2025-12-11 17:52:55 +0100

    update database configuration for production environment and enhance status API tests

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,7 +3,8 @@ import database from "infra/database";
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
   const databaseVersionResult = await database.query("SHOW server_version;");
-  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+  const databaseVersionValue =
+    databaseVersionResult.rows[0].server_version.split(" ")[0];
 
   const databaseMaxConnections = await database.query("SHOW max_connections;");
   const databaseMaxConnectionsValue =
